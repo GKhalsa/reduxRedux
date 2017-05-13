d8a6b170cebf5ed677f2de044b219be4bf436481
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2017-05-13 15:56:49 -0500

    add initial combine reducers test

--- spec/combineReducers.spec.js
+++ spec/combineReducers.spec.js
@@ -1,11 +1,17 @@
 import Store from '../src/store';
 import combineReducers from '../src/combineReducers';
-import 
+import {countReducer, toDoReducer} from './helpers/reducers';
 
 describe('combineReducers', () => {
 
   it('returns a combined reducer object that maps state keys to reducers', () => {
-    const reducer = combineReducers()
+    const reducer = combineReducers({
+      countReducer,
+      toDoReducer
+    });
+
+    expect(reducer).toEqual({countReducer: 0, toDoReducer: []});
+
   });
 
 });
