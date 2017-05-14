bd1917b8e8218db1209063b160182924a8678497
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2017-05-14 11:14:36 -0500

    add bindActionCreator test

---
+++ spec/bindActionCreator.spec.js
@@ -0,0 +1,14 @@
+import Store from '../src/store';
+import {countReducer} from './helpers/reducers';
+import bindActionCreator from '../src/bindActionCreator'
+import {add} from './helpers/actionCreators';
+
+describe('bindActionCreator', () => {
+  it('combines action with dispatching', () => {
+    const store = new Store(countReducer, 0);
+    const action = add;
+    const addDispatch = bindActionCreator(action, store.dispatch);
+    addDispatch(1);
+    expect(store.getState()).toEqual(1);
+  });
+});
